f23951666264f61032dfb319d61cc68981c72775
Welton Felix <contato.weltonf@gmail.com>
2021-04-07 02:47:26 -0300

    Create simple PyQt vertical layout example

---
+++ v_layout.py
@@ -0,0 +1,17 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QVBoxLayout, QPushButton, QWidget
+
+app = QApplication(sys.argv)
+
+window = QWidget()
+window.setWindowTitle("QVBoxLayout")
+
+layout = QVBoxLayout()
+layout.addWidget(QPushButton('Top'))
+layout.addWidget(QPushButton('Center'))
+layout.addWidget(QPushButton('Bottom'))
+window.setLayout(layout)
+
+window.show()
+sys.exit(app.exec_())
