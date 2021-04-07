4bcf9288cc89a9bcf7b4cdd911a7db4102ebc4fb
Welton Felix <contato.weltonf@gmail.com>
2021-04-07 10:57:14 -0300

    Create simple PyQt dialog example

---
+++ dialog.py
@@ -0,0 +1,34 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QDialog, QDialogButtonBox, QFormLayout, QLineEdit, QVBoxLayout
+
+class Dialog(QDialog):
+	def __init__(self, parent=None):
+		super().__init__(parent)
+
+		self.setWindowTitle('Dialog')
+
+		dialogLayout = QVBoxLayout()
+
+		formLayout = QFormLayout()
+		formLayout.addRow('Name:', QLineEdit())
+		formLayout.addRow('Age:', QLineEdit())
+		formLayout.addRow('Job:', QLineEdit())
+		formLayout.addRow('Hobbies:', QLineEdit())
+
+		dialogLayout.addLayout(formLayout)
+
+		buttons	= QDialogButtonBox()
+		buttons.setStandardButtons(QDialogButtonBox.Cancel | QDialogButtonBox.Ok)
+
+		dialogLayout.addWidget(buttons)
+
+		self.setLayout(dialogLayout)
+
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+
+	dialog = Dialog()
+	dialog.show()
+
+	sys.exit(app.exec_())
